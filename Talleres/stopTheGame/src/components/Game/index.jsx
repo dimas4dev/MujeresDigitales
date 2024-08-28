1a16f5dc075896d9c1859d65937567727657d5d1
Dimas Mendoza <dimasmendoza@habi.co>
2024-08-28 18:03:50 -0500

    feat: Mejorar diseño y funcionalidad del juego de Stop

--- Talleres/stopTheGame/src/components/Game/index.jsx
+++ Talleres/stopTheGame/src/components/Game/index.jsx
@@ -1,12 +1,20 @@
 // Game.js
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { COLUMNS } from '../../constants';
 import Columna from '../core/Column';
 import Fila from '../core/Row';
+import { getRandomLetter } from '../../utils';
 
 
 const Game = () => {
     const [rows, setRows] = useState([]);
+    const [currentLetter, setCurrentLetter] = useState('');
+
+    useEffect(() => {
+        // Seleccionar una letra aleatoria al iniciar el juego o una nueva ronda
+        const letter = getRandomLetter();
+        setCurrentLetter(letter);
+    }, []);
 
     const handleAddRow = () => {
         setRows([...rows, {}]);
@@ -15,16 +23,19 @@ const Game = () => {
     return (
         <div className="game-container">
             <h1>Juego de Stop</h1>
+
             <div className="column-container">
                 {COLUMNS.map((column) => (
                     <Columna key={column.id} name={column.name} />
                 ))}
             </div>
+
             <div className="rows-container">
                 {rows.map((_, index) => (
-                    <Fila key={index} columns={COLUMNS} />
+                    <Fila key={index} columns={COLUMNS} currentLetter={currentLetter} />
                 ))}
             </div>
+
             <button onClick={handleAddRow}>Agregar Fila</button>
         </div>
     );
--- Talleres/stopTheGame/src/components/core/Row/index.jsx
+++ Talleres/stopTheGame/src/components/core/Row/index.jsx
@@ -1,16 +1,25 @@
-const Fila = ({ columns }) => {
+
+const Fila = ({ columns, currentLetter }) => {
     return (
         <div className="fila">
-            {columns.map((column) => (
-                <input
-                    key={column.id}
-                    type="text"
-                    placeholder={column.name}
-                    className="input-field"
-                />
+            {columns.map((column, index) => (
+                index === 0 ? (
+                    // Mostrar la letra seleccionada en la primera columna
+                    <div key={column.id} className="letter-field">
+                        {currentLetter}
+                    </div>
+                ) : (
+                    <input
+                        key={column.id}
+                        type="text"
+                        placeholder={column.name}
+                        className="input-field"
+                    />
+                )
             ))}
         </div>
     );
 };
 
 export default Fila;
+
